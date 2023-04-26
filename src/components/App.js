93306250323e476a3737efe12eabbb27f49af7a4
sanaulla0 <vadapallisanaulla@gmail.com>
2023-04-26 22:08:37 +0530

    submitting code-Wed Apr 26 2023 22:08:35 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [mail , setMail] = useState('');
+  const[password , SetPassword] = useState('');
+  const[data , setData] = useState(false);
+  const handleclick = (event)=>{
+      event.preventDefault();
+       if(mail!='' && password!=''){
+            setData(true);
+       }
+  }
+     
   return (
     <div>
-        {/* Do not remove the main div */}
+
+     <h1>Parent Component</h1>
+     
+     <div>
+     {data ? ('Login Succesfull') : (
+     <form onSubmit={handleclick}>
+ <label>UserName :
+ </label>
+ <input type="text" onChange={((e)=>setMail(e.target.value))} />
+ <br/> <br/>&nbsp;&nbsp;
+ <label>Password :</label>
+ <input type="password" onChange={(e)=>SetPassword(e.target.value)} />
+ <br/>&nbsp;&nbsp;<br/>
+ <button>Login</button>
+
+     </form>
+     )}
+
+     </div>
+    
+         
     </div>
   )
 }
